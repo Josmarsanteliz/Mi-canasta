8fd27fdf7e3ba981d5865166c9d39f968ed8f5fd
Josmar <josmarsanteliz@gmail.com>
2026-09-14 19:33:34 -0400

    fix: añade adaptador de Netlify y config server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   // Agrega esta línea para habilitar el modo SSR en todo el proyecto
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: netlify(),
     vite: {
     plugins: [tailwindcss()]
   }
